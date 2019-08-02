09413d2b52e602f99cf76d0a9a4f109142232678
Kevin Devlin <37345236+KevinDevlln@users.noreply.github.com>
2019-08-02 10:56:25 +0100

    Update ProjectEuler3.py

--- ProjectEuler3.py
+++ ProjectEuler3.py
@@ -2,7 +2,7 @@ def Euler3(n=13195):
     for i in range(2,13195):
         while n % i == 0:
             n //= i
-            print("Yay, %d is a factor, now we should test %d" % (i, n))
+            print("%d is a factor, now we should test %d" % (i, n))
             if n == 1 or n == i:
                 return i
 
